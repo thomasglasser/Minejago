8951eea0c60224f225d95334ba96ce944a32536b
Thomas Glasser <thomas@thomasglasser.dev>
2025-04-05 05:58:51 -0400

    Switched SkulkinRaidType to use functional interfaces for fields

--- src/main/java/dev/thomasglasser/minejago/world/entity/skulkin/raid/SkulkinRaidType.java
+++ src/main/java/dev/thomasglasser/minejago/world/entity/skulkin/raid/SkulkinRaidType.java
@@ -1,22 +1,36 @@
 package dev.thomasglasser.minejago.world.entity.skulkin.raid;
 
-import java.util.function.BiFunction;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.level.ServerLevel;
-import org.apache.commons.lang3.function.TriFunction;
 import org.jetbrains.annotations.Nullable;
 
-public record SkulkinRaidType(TriFunction<ServerLevel, Integer, BlockPos, AbstractSkulkinRaid> createConstructor, BiFunction<ServerLevel, CompoundTag, AbstractSkulkinRaid> loadConstructor, BiFunction<ServerLevel, BlockPos, @Nullable BlockPos> centerFunction) {
+public record SkulkinRaidType(Creator creator, Loader loader, CenterFinder centerFinder) {
+
     public AbstractSkulkinRaid create(ServerLevel level, Integer id, BlockPos center) {
-        return this.createConstructor.apply(level, id, center);
+        return this.creator.create(level, id, center);
     }
 
     public AbstractSkulkinRaid load(ServerLevel level, CompoundTag compound) {
-        return this.loadConstructor.apply(level, compound);
+        return this.loader.load(level, compound);
     }
 
     public @Nullable BlockPos findValidRaidCenter(ServerLevel level, BlockPos pos) {
-        return this.centerFunction.apply(level, pos);
+        return this.centerFinder.findValidRaidCenter(level, pos);
+    }
+    @FunctionalInterface
+    public interface Creator {
+        AbstractSkulkinRaid create(ServerLevel level, Integer id, BlockPos center);
+    }
+
+    @FunctionalInterface
+    public interface Loader {
+        AbstractSkulkinRaid load(ServerLevel level, CompoundTag compound);
+    }
+
+    @FunctionalInterface
+    public interface CenterFinder {
+        @Nullable
+        BlockPos findValidRaidCenter(ServerLevel level, BlockPos pos);
     }
 }
